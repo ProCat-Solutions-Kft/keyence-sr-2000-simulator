52f68ec4f83835aac4a77e1ea2b92d730a34ee08
vincze.jozsef <vincze.jozsef.develop@gmail.com>
2024-05-15 13:54:40 +0200

    add env

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const net = require('net');
 const http = require('http');
-
-// Create an Express application
+require('dotenv').config();
 const app = express();
-
+var fs = require('fs');
 // HTTP Server setup
 const server = http.createServer(app);
-const PORT_HTTP = 3000;
+const PORT_HTTP = process.env.HTTP_PORT || 3000;
 
 // Serve a basic HTTP response
 app.get('/', (req, res) => {
@@ -16,48 +15,52 @@ app.get('/', (req, res) => {
 
 // UDP Server setup for PORT 9015
 const udpServer9015 = require('dgram').createSocket('udp4');
-const PORT_UDP_9015 = 9015;
-const PORT_UDP_9004 = 9004;
+const PORT_UDP = process.env.PORT_UDP || 9015;
+const PORT_TCP = process.env.PORT_TCP || 9004;
 
 udpServer9015.on('error', (err) => {
-  console.log(`UDP Server 9015 error:\n${err.stack}`);
+  console.log(`UDP Server ${PORT_UDP} error:\n${err.stack}`);
   udpServer9015.close();
 });
 
 udpServer9015.on('message', (msg, rinfo) => {
   const messageText = msg.toString().trim();
-  console.log(`Server 9015 Received ${msg.length} bytes from ${rinfo.address}:${rinfo.port}: ${messageText}`);
+  console.log(`Server ${PORT_UDP} Received ${msg.length} bytes from ${rinfo.address}:${rinfo.port}: ${messageText}`);
 
   switch (messageText) {
     case 'READER':
     case 'READER2':
-      sendResponse(`OK,${messageText},keyence SR-2000,keyence SR-2000AAAAAAAAAAAAAA`, rinfo, udpServer9015);
+      sendResponse(`OK,${messageText},${process.env.DEVICE_INFO}`, rinfo, udpServer9015);
       break;
     default:
-      console.log('Server 9015: Unknown command received');
+      console.log(`Server ${PORT_UDP}: Unknown command received`);
       break;
   }
 });
 
 udpServer9015.on('listening', () => {
   const address = udpServer9015.address();
-  console.log(`UDP Server 9015 listening ${address.address}:${address.port}`);
+  console.log(`UDP Server ${PORT_UDP} listening ${address.address}:${address.port}`);
 });
 
-udpServer9015.bind({ address: '10.8.0.13', port: PORT_UDP_9015 });
+udpServer9015.bind({ address: process.env.HOST_IP, port: PORT_UDP });
 
 // TCP Server setup for PORT 9004
 const tcpServer9004 = net.createServer((socket) => {
   console.log(`TCP Server 9004 connected: ${socket.remoteAddress}:${socket.remotePort}`);
 
+  const code = JSON.parse(fs.readFileSync(process.env.CODES_FILE_PATH, 'utf8'));
+
   socket.on('data', (data) => {
     const messageText = data.toString().trim();
-    console.log(`Server 9004 Received ${data.length} bytes from ${socket.remoteAddress}:${socket.remotePort}: ${messageText}`);
+    console.log(`Server ${PORT_TCP} Received ${data.length} bytes from ${socket.remoteAddress}:${socket.remotePort}: ${messageText}`);
 
     if (messageText === 'LON') {
-      sendResponse('ABCDEFGH:A', { address: socket.remoteAddress, port: socket.remotePort }, socket);
+      sendResponse(code?.codes[Math.floor(Math.random()*code?.codes.length)] + '\r', { address: socket.remoteAddress, port: socket.remotePort }, socket);
+    } else if (messageText === 'LOFF') {
+      sendResponse('ERROR\r', { address: socket.remoteAddress, port: socket.remotePort }, socket);
     } else {
-      console.log('Server 9004: Unknown command received');
+      console.log(`Server ${PORT_TCP}: Unknown command received`);
     }
   });
 
@@ -68,14 +71,14 @@ const tcpServer9004 = net.createServer((socket) => {
 
 tcpServer9004.on('listening', () => {
   const address = tcpServer9004.address();
-  console.log(`TCP Server 9004 listening ${address.address}:${address.port}`);
+  console.log(`TCP Server ${PORT_TCP} listening ${address.address}:${address.port}`);
 });
 
 tcpServer9004.on('error', (err) => {
-  console.error(`TCP Server 9004 error: ${err}`);
+  console.error(`TCP Server ${PORT_TCP} error: ${err}`);
 });
 
-tcpServer9004.listen(PORT_UDP_9004, '10.8.0.13');
+tcpServer9004.listen(PORT_TCP, process.env.HOST_IP);
 
 // Function to send responses via specified UDP server or TCP socket
 function sendResponse(responseMessage, rinfo, serverOrSocket) {
@@ -96,6 +99,6 @@ function sendResponse(responseMessage, rinfo, serverOrSocket) {
 }
 
 // Start HTTP Server on all interfaces
-server.listen(PORT_HTTP, '0.0.0.0', () => {
-  console.log(`HTTP Server running on http://0.0.0.0:${PORT_HTTP}`);
+server.listen(PORT_HTTP, process.env.HOST_IP, () => {
+  console.log(`HTTP Server running on ${process.env.HOST_IP}:${PORT_HTTP}`);
 });
